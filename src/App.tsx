dc020dd2d1f1a4d1d632bd22bd16fde5fc05e498
Viet Anh Ngo <ngovietanh503@gmail.com>
2022-03-16 15:10:58 +0700

    feat: Create layout admin & website

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import './App.css'
 import ShowInfo from './components/ShowInfo'
 import type { ProductType } from './types/product';
 import { list, remove } from './api/product';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import WebsiteLayout from './pages/layouts/WebsiteLayout';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import AdminLayout from './pages/layouts/AdminLayout';
+import Dashboard from './pages/Dashboard';
+import ManagerProduct from './pages/ManagerProduct';
 function App() {
   const [products, setProducts] = useState<ProductType[]>([]);
   // const [count, setCount] = useState<number>(0);
@@ -14,6 +21,7 @@ function App() {
         const { data } = await list();
         setProducts(data);
      }
+     console.log(products);
      getProducts();
   },[])
 
@@ -21,31 +29,53 @@ function App() {
     // xoa tren API
     const { data } = await remove(id);
     // reRender
-    data && setProducts(products.filter(item => item._id !== data._id));
+    data && setProducts(products.filter(item => item.id !== data.id));
   }
   return (
     <div className="App">
-      <table>
+      {/* <table>
         <thead>
           <th>#</th>
           <th>Name</th>
           <th></th>
         </thead>
         <tbody>
-          {products.map((item, index) => {
+          {products && products.map((item, index) => {
             return <tr>
                     <td>{index + 1}</td>
                     <td>{item.name}</td>
-                    
                     <td>
-                      <button onClick={() => removeItem(item._id)}>Remove</button>
+                      <button onClick={() => removeItem(item.id)}>Remove</button>
                     </td>
                   </tr>
           })}
           
         </tbody>
-      </table>
-      
+      </table> */}
+      <header>
+        <ul>
+          <li><NavLink to="/">Home page</NavLink></li>
+          <li><NavLink to="/product">Product</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+        </ul>
+      </header>
+      <main>
+      <Routes>
+        {/* <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="product" element={<h1>Product page</h1>} />
+        <Route path="about" element={<h1> About page </h1>} /> */}
+        <Route path="/" element={<WebsiteLayout/>}>
+            <Route index element={<Home/>} />
+            <Route path="product" element={<Product/>} />
+        </Route>
+
+        <Route path="admin" element={<AdminLayout/>}>
+            <Route index element={<Navigate to="Dashboard"/>} />
+            <Route path="Dashboard" element={<Dashboard/>} />
+            <Route path="product" element={<ManagerProduct/>} />
+        </Route>
+      </Routes>
+      </main>
     </div>
   )
 }
