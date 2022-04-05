d140605e4ec0df62ce4d2474946b9e4a90154bb2
Viet Anh Ngo <ngovietanh503@gmail.com>
2022-04-06 00:58:12 +0700

    hello

--- src/api/instance.tsx
+++ src/api/instance.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'http://localhost:3001' 
+    //khi chạy server thì đéo đky đnhap được
+    // baseURL: 'http://localhost:3001/api' khi đky đnhap thì đéo lên server
 });
 export default instance;
