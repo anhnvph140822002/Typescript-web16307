e03e011298c54fb796f16b5388ed1dc694cb54de
Viet Anh Ngo <ngovietanh503@gmail.com>
2022-04-08 15:10:17 +0700

    Category

---
+++ src/api/category.tsx
@@ -0,0 +1,24 @@
+
+import { ProductType } from '../types/product';
+import instance from './instance';
+
+export const list = () => {
+    const url = '/products';
+    return instance.get(url);
+}
+export const remove = (id: number) => {
+    const url = `/products/${id}`;
+    return instance.delete(url);
+}
+export const add = (products: ProductType) => {
+    const url = `/products`;
+    return instance.post(url, products);
+}
+export const update = (products: ProductType) => {
+    const url = `/products/${products.id}`;
+    return instance.put(url, products);
+}
+export const read = (id: number) => {
+    const url = `/products/${id}`;
+    return instance.get(url);
+}
---
+++ src/pages/categoryAdd.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const categoryAdd = () => {
+  return (
+    <div>categoryAdd</div>
+  )
+}
+
+export default categoryAdd
